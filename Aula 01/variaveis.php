041afd131fd02fbb007aff316662f58b1f894200
jefferson <jecorrea.2008@gmail.com>
2025-08-01 16:47:54 -0300

    atividade: aula 1 variaveis

---
+++ Aula 01/variaveis.php
@@ -0,0 +1,26 @@
+
+<?php
+    echo "meu nome é jefferson\n";
+    print "minha idade é 20";
+?>
+
+<?php
+    $string = "nome";
+    $int = "17";
+?>
+
+<?php
+$nome = "jefferson";
+$int = "17";
+echo "meu nome é: $nome e minha idade é: $int"
+?>
+
+<?php
+echo "olá! \n";
+$nome = "jefferson \n";
+$idade = "17";
+$ano_atual = "2025";
+
+$data_nasc = $ano_atual-$idade;
+echo $nome, "você nasceu em: $data_nasc";
+?>                                                                                                 
