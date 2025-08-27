5e003691d38503802e5129073e6a45d4d183b3a5
jefferson-kkk <jecorrea.2008@gmail.com>
2025-08-27 16:56:14 -0300

    Add files via upload

---
+++ getter_setter.php
@@ -0,0 +1,46 @@
+<?php
+class pessoa{
+    private $nome;
+    private $cpf;
+    private $telefone;
+    private $idade;
+    private $email;
+    private $senha;
+
+    //crie o constructor para a classe pessoa.
+    private function __construct($nome, $cpf, $telefone, $idade, $email, $senha) {
+        $this ->setnome($nome);
+        $this ->setcpf($cpf);
+        $this ->settelefone($telefone);
+        $this ->setidade($idade);
+        $this ->email=$email;
+        $this ->senha=$senha;
+    }
+ public function setnome($nome){
+    $this ->nome=ucwords(strtolower($nome));
+    }
+    public function getnome(){
+        return $this ->nome;
+    }
+    public function setcpf($cpf){
+    $this ->cpf = preg_replace('/\D/',"", $cpf);
+    }
+    public function getCpf(){
+        return $this ->cpf;
+}
+    public function settelefone($telefone) {
+    $this ->telefone = preg_replace('/\D/', "", $telefone);
+} public function gettelefone(){
+    return $this ->telefone;
+} public function setidade($idade){
+    $this ->idade = abs((int)$idade);
+} public function getidade(){
+    return $this -> idade;
+}
+}
+
+$aluno1 = new pessoa(" JeFF", "376-756-509-43", "19-7546-6540", -18, "jeff.2008@gmail.com ", "12345");
+
+echo $aluno1 ->getnome();
+
+?>
