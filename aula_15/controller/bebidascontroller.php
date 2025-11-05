fc3b6e536e63664b03e6e3ef770f3a31586d961d
jefferson-kkk <jecorrea.2008@gmail.com>
2025-11-05 16:54:00 -0300

    atividade 05/11/2025

---
+++ aula_15/controller/bebidascontroller.php
@@ -0,0 +1,29 @@
+<?php
+// CORRIGIR ESTAS LINHAS:
+require_once __DIR__ . '/../modelo/BebidaDAO.php';
+require_once __DIR__ . '/../modelo/bebidas.php';
+
+class BebidaController {
+    private $dao;
+
+    public function __construct() {
+        $this->dao = new BebidaDAO();
+    }
+
+    public function ler() {
+        return $this->dao->lerBebidas();
+    }
+
+    public function criar($nome, $categoria, $volume, $valor, $qtde) {
+        $bebida = new Bebida($nome, $categoria, $volume, $valor, $qtde);
+        $this->dao->criarBebida($bebida);
+    }
+
+    public function atualizar($nome, $valor, $qtde) {
+        $this->dao->atualizarBebida($nome, $valor, $qtde);
+    }
+
+    public function deletar($nome) {
+        $this->dao->excluirBebida($nome);
+    }
+}
